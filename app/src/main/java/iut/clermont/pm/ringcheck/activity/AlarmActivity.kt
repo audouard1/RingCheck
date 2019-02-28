559f1f5bd1ee4d866e6cfff5805de89c206e8b24
Unknown <aurelien.douard@lojelis.com>
2019-02-28 22:52:46 +0100

    feat : sound on alarm sound volume, image load on alarmActivity

--- app/src/main/java/iut/clermont/pm/ringcheck/activity/AlarmActivity.kt
+++ app/src/main/java/iut/clermont/pm/ringcheck/activity/AlarmActivity.kt
@@ -1,13 +1,20 @@
 package iut.clermont.pm.ringcheck.activity
 
-import android.os.Build
-import android.os.Bundle
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.os.*
 import android.view.WindowManager
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import iut.clermont.pm.ringcheck.R
 import iut.clermont.pm.ringcheck.viewmodel.RingViewModel
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_alarm.*
+
 
 class AlarmActivity : AppCompatActivity() {
 
@@ -17,19 +24,55 @@ class AlarmActivity : AppCompatActivity() {
 
     private lateinit var viewModel: RingViewModel
 
+    private lateinit var ringtone : Ringtone
+
+    private val handler = Handler()
+
+    private lateinit var runnable : Runnable
+
+    private lateinit var vibrator : Vibrator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         setContentView(R.layout.activity_alarm)
+        val imageView = findViewById<ImageView>(R.id.catImageView)
+        val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        ringtone = RingtoneManager.getRingtone(this, uri)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ringtone.audioAttributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+        }
+
+        runnable = object : Runnable {
+            override fun run() {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                        vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+                    else
+                        vibrator.vibrate(500)
+                ringtone.play()
+                handler.postDelayed(this, 1000)
+            }
+        }
+        handler.post(runnable)
+
         viewModel = ViewModelProviders.of(this).get(RingViewModel::class.java)
-        val a = viewModel.cat.observe(this, Observer { cat ->
+        viewModel.cat.observe(this, Observer { cat ->
             val res = cat
-            //val url = cat.url
+            if (res.first().url != ""){
+                Glide.with(this).load(res.first().url).into(imageView)
+            }
         })
+        stopAlarm.setOnClickListener { finish() }
 
     }
 
-
-
+    override fun onDestroy() {
+        ringtone.stop()
+        handler.removeCallbacks(runnable)
+        super.onDestroy()
+    }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
